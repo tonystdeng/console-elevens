e8def78af1c0f46279a2401308a4827711108f59
tonystdeng <tony.st.deng@gmail.com>
2026-09-16 18:48:36 -0400

    program.cs documented

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-﻿// Author:
+﻿// Author: Tony Deng
 // File Name: Program.cs
-// Project Name: 
-// Creation Date: 
-// Modified Date: 
-// Description: 
+// Project Name: PASS 1 - Elevens
+// Creation Date: 09/16/26
+// Modified Date: 09/25/26
+// Description: a fully playable version of the card game, Elevens in a 120x30 character terminal with dotnet
 
 namespace PASS1;
 
