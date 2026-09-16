021d0dd1811f20c020300ecb85fb0d53167c0fe5
tonystdeng <tony.st.deng@gmail.com>
2026-09-16 18:42:06 -0400

    finished setup

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Author:
+// File Name: Program.cs
+// Project Name: 
+// Creation Date: 
+// Modified Date: 
+// Description: 
+
+namespace PASS1;
+
+class Program
+{
+    // const var
+    // global var
+    static void Main(string[] args)
+    {
+        // local var
+        // intro out
+        Console.WriteLine("Hello, World!");
+        // input
+        // processing
+        // out
+    }
+}
---
+++ git.bash
@@ -0,0 +1,13 @@
+# git init
+# git branch -M main
+# git remote add origin <link>
+# git remote -v
+
+# git fetch
+# git merge origin/main --allow-unrelated-histories
+
+git rm --cached -r .
+git add .
+git commit -m "${*:-unamed commit}"
+
+git push -u origin main
